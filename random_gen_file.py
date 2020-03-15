b1492b920a296047b34e2a64bbee091ec47a2804
Jai-Wilson <61450537+Jai-Wilson@users.noreply.github.com>
2020-03-15 21:26:13 +1000

    Delete random_gen_file.py

--- random_gen_file.py
+++
@@ -1,8 +0,0 @@
-import random
-
-value = 20
-
-for i in range(value):
-    random_number = random.uniform(-200, 200)
-    file = open('temps_input.txt', 'a')
-    file.write('{}\n'.format(random_number))
