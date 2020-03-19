513892c604e4c45142cb32579702dc139a579fb7
jaiwil1 <jai.wilson@my.jcu.edu.au>
2020-03-19 18:31:48 +1000

    add list comprehensions file

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,13 +5,13 @@ numbers = []
 
 for i in range(5):
     number = int(input("Number: "))
-    numbers.append(number)
+    numbers.append(number) #add entered numbers to the list created
 
 print("The first number is {}".format(numbers[0]))
 print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is ", sum(numbers) / len(numbers))
+print("The average of the numbers is ", sum(numbers) / len(numbers)) # print respective outcomes
 
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup',
@@ -19,7 +19,7 @@ usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup',
              'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
              'InterpreterInterface', 'StartServer', 'bob']
 
-given_username = input("What is your username? ")
+given_username = input("What is your username? ") #check if the entered username is a username stored on the system
 if given_username in usernames:
     print("Access granted")
 else:
