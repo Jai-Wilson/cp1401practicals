4550d19284f9c18b909f196a8b764a94dfd61b93
jaiwil1 <jai.wilson@my.jcu.edu.au>
2020-03-19 19:13:24 +1000

    add git ignore file

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,16 +1,16 @@
 import random
 
 
-picks = int(input('How many picks would you like to generate? '))
+picks = int(input('How many picks would you like to generate? ')) #generate the amount of quick picks
 total = 6
 values = []
 for i in range(picks):
     count = 0
     random_values = []
     while count < total:
-        random_number = random.randrange(1, 46)
-        random_values.append(random_number)
+        random_number = random.randrange(1, 46) #generate random values
+        random_values.append(random_number) #add to list
         count +=1
-    random_values.sort()
-    values.append(random_values)
-    print(values[i])
+    random_values.sort() #sort the values in acending order
+    values.append(random_values) #add to values list
+    print(values[i]) #print the respective line of the code
