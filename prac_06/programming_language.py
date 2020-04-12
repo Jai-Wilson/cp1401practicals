08675db60f58192603d397a768fa7b4c2f934e1a
jaiwil1 <jai.wilson@my.jcu.edu.au>
2020-04-12 15:29:10 +1000

    Add programming language file with added fields

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,2 +1,8 @@
 """Programming Language file"""
 
+class ProgrammingLanguage:
+    def __init__(self, field='', typing, reflection, year=0):
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
