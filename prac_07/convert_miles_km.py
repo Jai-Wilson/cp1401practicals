e022ce1e56275ff0ece1a0f4fcf58f16953a4eb3
jaiwil1 <jai.wilson@my.jcu.edu.au>
2020-04-24 12:51:48 +1000

    Add docstrings to functions in convert miles program

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -10,23 +10,28 @@ class ConvertMilesToKm(App):
     Distance = StringProperty()
 
     def build(self):
+        '''Build the kivy app from the kv file'''
 
         self.title = 'Convert Miles to Kilometers'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_convert(self):
+        '''Convert user entered miles to kilometers'''
         distance_in_km = self.get_valid_miles()
         distance_in_miles = distance_in_km * MILES_TO_KM
         self.root.ids.output_distance.text = str(distance_in_miles)
 
     def handle_change(self, change):
+        '''Convert user miles to kilomeers when the up/down button is pressed, increasing and
+        decreasing the user input by 1'''
         distance_in_km = self.get_valid_miles() + change
         self.root.ids.input_distance.text = str(distance_in_km)
         self.handle_convert()
 
 
     def get_valid_miles(self):
+        '''check user input to be valid float variable. If not, use 0'''
         try:
             user_input = float(self.root.ids.input_distance.text)
             return user_input
