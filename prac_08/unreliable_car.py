facb1ca1221fac31f47cd20677136192fe20b9b6
jaiwil1 <jai.wilson@my.jcu.edu.au>
2020-05-07 11:36:04 +1000

    Add unreliable car class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,5 +1,19 @@
+from random import randint
 from prac_08.car import Car
 
-class UnreliableCar(Car)
+class UnreliableCar(Car):
 
-    
+    def __init__(self, name, fuel, reliability):
+        """Initialise an unreliable car instance, based on parent class Car"""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+
+    def drive(self, distance):
+        rand_num = randint(0, 100)
+        if rand_num >= self.reliability:
+            distance = 0
+
+        distance_driven = super().drive(distance)
+        return distance_driven
+
